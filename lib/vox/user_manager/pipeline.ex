878a72c74ab0610b89e0718e85d2a98b42ff1ab6
Maciej Moroz <maciek@Maciejs-MacBook-Pro-2.local>
2022-05-09 14:54:35 +0200

    pre- delete leftover user templates

--- lib/vox/user_manager/pipeline.ex
+++ lib/vox/user_manager/pipeline.ex
@@ -4,5 +4,7 @@ defmodule Vox.UserManager.Pipeline do
   error_handler: Vox.UserManager.ErrorHandler,
   module: Vox.UserManager.Guardian
 
-
+  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+  plug Guardian.Plug.LoadResource, allow_blank: true
 end
